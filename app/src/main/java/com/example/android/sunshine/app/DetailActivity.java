f8915c7edbb55398f019b203e76d685aba23582f
Amanda Farrell <bluepandadev@gmail.com>
2017-03-08 22:15:45 -0700

    Display forecast string in the DetailActivity

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -10,6 +12,13 @@ public class DetailActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Read the intent string passed in from ForecastFragment
+        Intent intent = getIntent();
+        String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        TextView textView = (TextView) findViewById(R.id.detail_forecast);
+        textView.setText(forecast);
     }
 
 }
